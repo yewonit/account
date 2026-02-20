0b92994de6f72485ab123f6562ffda7d43f460a0
isaac <isaac@tpirates.com>
2026-02-20 15:36:46 +0900

    fix: cicd

--- config/environment.js
+++ config/environment.js
@@ -5,27 +5,27 @@ config()
 
 const environment = {
 	local: {
-		REDIS_HOST: process.env.LOCAL_REDIS_HOST || "localhost",
-		REDIS_PORT: process.env.LOCAL_REDIS_PORT || 6379,
+		REDIS_HOST: "localhost",
+		REDIS_PORT: 6379,
 		JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
 		SENDER_EMAIL: process.env.SENDER_EMAIL,
 		SENDER_SECRET: process.env.SENDER_SECRET
 	},
 	development: {
-		REDIS_HOST: process.env.DEV_REDIS_HOST,
-		REDIS_PORT: process.env.DEV_REDIS_PORT,
+		REDIS_HOST: process.env.REDIS_HOST,
+		REDIS_PORT: process.env.REDIS_PORT,
 		JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
 		SENDER_EMAIL: process.env.SENDER_EMAIL,
 		SENDER_SECRET: process.env.SENDER_SECRET
 	},
 	production: {
-		REDIS_HOST: process.env.PROD_REDIS_HOST,
-		REDIS_PORT: process.env.PROD_REDIS_PORT,
+		REDIS_HOST: process.env.REDIS_HOST,
+		REDIS_PORT: process.env.REDIS_PORT,
 		JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
 		SENDER_EMAIL: process.env.SENDER_EMAIL,
 		SENDER_SECRET: process.env.SENDER_SECRET
 	},
 }
 
-const nodeEnv = process.env.NODE_ENV || "local"
+const nodeEnv = process.env.NODE_ENV || "development"
 export default environment[nodeEnv]
